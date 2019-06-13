6b78e095c96c7f156e66e5fcfdac888981a70a77
unknown <NoemiYanqui>
2019-06-13 10:25:49 -0500

    subiendo lab7

---
+++ lab07/dawa_api/lib/utils.js
@@ -0,0 +1,31 @@
+const jwt= require('jsonwebtoken');
+
+process.env.JWT_SECRET ="noemi";
+
+module.exports = {
+    generateToken: user =>{
+        const u = {
+            _id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+        };
+        return token =jwt.sign(u, process.env.JWT_SECRET,{
+            expiresIn: 60*60*24
+        });
+    },
+    verifyToken: token=>{
+        return new Promise((resolve, reject)=>{
+            jwt.verify(token, process.env.JWT_SECRET, (err,user)=>{
+                if(err){
+                    reject(err);
+                }
+                resolve(user);
+            });
+        });
+    },
+    getCleanUser: user =>{
+        const {password, age, createdAt, updateAt, __v, ...exposedData }=user;
+        return exposedData;
+    }
+};
---
+++ lab07/lab07/src/views/Details.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+const Details = ()=>{
+    return (<div>
+        <h1>Estos son los detalles : )</h1>
+    </div>);
+};
+
+export default Details;
